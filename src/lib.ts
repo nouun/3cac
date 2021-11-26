6fa379411003fbe6f9193c2cd96cbf980fb5f57e
nouun <me@nouun.dev>
2021-11-26 16:57:14 +1300

    fix: remove KuCoin filter

--- src/lib.ts
+++ src/lib.ts
@@ -4,11 +4,6 @@ const CB_API = "https://cryptobubbles.net/backend/data/currentBubbles500.json";
 
 export const fetchBubbles = async (): Promise<Bubble[]> => {
   const res = await fetch(CB_API);
-  const bubbles = <Bubble[]>await res.json();
-
-  const out = bubbles
-    .filter((bubble) => !!bubble.kucoinSymbol);
-
-  return out;
+  return await res.json();
 };
 
